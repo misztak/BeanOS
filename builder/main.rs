54e8584e890daee0cdf13243b785728d3c972a7a
misztak <szostak.michael@gmail.com>
2022-12-06 01:30:42 +0100

    builder: check filesize of bootloader and kernel after build

--- builder/main.rs
+++ builder/main.rs
@@ -3,8 +3,8 @@ A glorified build script for the OS and bootloader.
 
 */
 
-use std::{env, path::PathBuf, process::Command};
 use llvm_tools_build;
+use std::{env, path::PathBuf, process::Command};
 
 fn main() {
     let is_release_build = !cfg!(debug_assertions);
@@ -29,7 +29,10 @@ fn main() {
     // Step 1: Build the kernel
     //
 
-    println!("\n### Step 1: [Building BeanOS in {} mode] ###\n", build_type);
+    println!(
+        "\n### Step 1: [Building BeanOS in {} mode] ###\n",
+        build_type
+    );
 
     let project_root_dir = env::var("CARGO_MANIFEST_DIR")
         .expect("Environment variable 'CARGO_MANIFEST_DIR' undefined");
@@ -38,24 +41,29 @@ fn main() {
     let mut cmd = Command::new("cargo");
     cmd.current_dir(&kernel_dir);
     cmd.arg("build");
-    if is_release_build { cmd.arg("--release"); }
-    let cmd_status = cmd
-        .status()
-        .expect("Failed to run cargo to build kernel");
+    if is_release_build {
+        cmd.arg("--release");
+    }
+    let cmd_status = cmd.status().expect("Failed to run cargo to build kernel");
     assert!(cmd_status.success(), "Failed to build kernel");
 
     //
     // Step 2: Build the bootloader
     //
 
-    println!("\n### Step 2: [Building bootloader in {} mode] ###\n", build_type);
+    println!(
+        "\n### Step 2: [Building bootloader in {} mode] ###\n",
+        build_type
+    );
 
     let bootloader_dir = PathBuf::from(&project_root_dir).join("bootloader");
 
     let mut cmd = Command::new("cargo");
     cmd.current_dir(&bootloader_dir);
     cmd.arg("build");
-    if is_release_build { cmd.arg("--release"); }
+    if is_release_build {
+        cmd.arg("--release");
+    }
     let cmd_status = cmd
         .status()
         .expect("Failed to run cargo to build bootloader");
@@ -67,7 +75,8 @@ fn main() {
 
     println!("\n### Step 3: [Creating flat binary] ###\n");
 
-    let output_dir = bootloader_dir.join(format!("target/x86_64-bean_os_bootloader/{}", build_type));
+    let output_dir =
+        bootloader_dir.join(format!("target/x86_64-bean_os_bootloader/{}", build_type));
     let bootloader_elf = output_dir.join("bootloader");
 
     let mut bootloader_image = PathBuf::from(&bootloader_elf);
@@ -101,5 +110,50 @@ fn main() {
         panic!("Failed to create flat binary");
     }
 
+    // check the filesize of the bootloader and kernel inside the final ELF file
+    // only available on linux right now because it calls `readelf`
+    if cfg!(target_os = "linux") {
+        check_filesize(&bootloader_elf);
+    }
+
     println!("### DONE! ###");
+}
+
+#[cfg(target_os = "linux")]
+fn check_filesize(elf_path: &PathBuf) {
+    let mut cmd = Command::new("readelf");
+    cmd.arg("-t");
+    cmd.arg(&elf_path);
+    let output = cmd.output().expect("Failed to run readelf");
+    assert!(output.status.success(), "Readelf command failed");
+
+    let seg_info: Vec<&str> = std::str::from_utf8(&output.stdout)
+        .expect("Failed to parse readelf output")
+        .split('\n')
+        .collect();
+
+    let mut bootloader_size: u64 = 0;
+    let mut kernel_size: u64 = 0;
+
+    // yikes
+    for (i, &line) in seg_info.iter().enumerate() {
+        if line.contains(".bootloader") {
+            let size_str = seg_info[i + 2].trim().split_once(' ').unwrap().0;
+            bootloader_size = u64::from_str_radix(size_str, 16).unwrap();
+        }
+        if line.contains(".kernel") {
+            let size_str = seg_info[i + 2].trim().split_once(' ').unwrap().0;
+            kernel_size = u64::from_str_radix(size_str, 16).unwrap();
+        }
+    }
+
+    println!("Bootloader segment size: 0x{:x} ({} out of 480 KiB available)", bootloader_size, bootloader_size / 1024);
+    println!("Kernel segment size:     0x{:x} ({} KiB)", kernel_size, kernel_size / 1024);
+
+    if bootloader_size > 480 * 1024 {
+        eprintln!("\x1b[93mWARNING: Bootloader might be overflowing usable memory region!\x1b[0m");
+    }
+
+    println!();
+
 }
